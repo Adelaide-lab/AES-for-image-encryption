9a0ed320126d86cde155d6c97454584b2257efbb
Adelaide <73685042+Adelaide-lab@users.noreply.github.com>
2021-05-07 18:19:19 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
 #include "file_encrp.h"
 
-
-
 int main() { 
 
 	FILE *fpin,*fpout;
 
-	fpin=fopen("D:\\dachuang\\AES_picture\\c1.bmp","rb");
-	fpout=fopen("D:\\dachuang\\AES_picture\\c2.bmp","wb+");
+	fpin=fopen("D:\\c1.bmp","rb"); /*path to your original file*/
+	fpout=fopen("D:\\c2.bmp","wb+");/*path to the file you create*/
 
 	encrypt_to_bmp(fpin,fpout);
 	
 	fclose(fpin);
 	fclose(fpout);
 	
-	fpin=fopen("D:\\dachuang\\AES_picture\\c2.bmp","rb");
-	fpout=fopen("D:\\dachuang\\AES_picture\\c3.bmp","wb+");
+	fpin=fopen("D:\\c2.bmp","rb");
+	fpout=fopen("D:\\c3.bmp","wb+");
 	
 	deencrpt_to_bmp(fpout,fpin);
 
